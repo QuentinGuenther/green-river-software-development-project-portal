e3154bd29e0f2548b1574876e950d988a96c5b2d
Quentin Guenther <qguenther95@live.com>
2018-01-25 21:08:16 -0800

    Add login page along with home and error route

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+
+    // Turn on error reporting
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+
+    // Require autoload
+    require_once('vendor/autoload.php');
+
+    // Create fat-free instance
+    $f3 = Base::instance();
+
+    // Set debug level to dev
+    $f3->set('DEBUG', 3);
+
+    // Default route
+    $f3->route('GET /', function() {
+        $template = new Template();
+        echo $template->render('views/home.html');
+    });
+
+    // Login route
+    $f3->route('GET /login', function() {
+        echo Template::instance()->render('views/login.html');
+    });
+
+    // Error page
+    $f3->set('ONERROR', function($f3) {
+        echo Template::instance()->render('views/error.html');
+    });
+    
+    $f3->run();
+?>
