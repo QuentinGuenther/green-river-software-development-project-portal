805bd10e760f23be9bcda124f394b47d8e355bb0
Nathan Corbin <ncorbin94@live.com>
2018-02-12 17:33:12 -0500

    validations for new-class mostly complete

--- index.php
+++ index.php
@@ -51,8 +51,11 @@
 
     $f3->route('GET|POST /new-class', function($f3) {
 
+        $quarters = array("fall", "winter", "spring", "summer");
+        $f3->set("quarters", $quarters);
+
         if(isset($_POST['submit'])) {
-            include('models/new-class-validation');
+            include('models/new-class-validation.php');
 
             $errors = array();
 
@@ -65,15 +68,30 @@
             if(!validDate($_POST['year'])) 
                 $errors['year'] = "Invalid year: must be ".date('Y')." or greater";
 
+            if(!validGithubUrl($_POST['github']))
+                $errors['github'] = "Not a github url";
+
+            if(!validTrelloUrl($_POST['trello']))
+                $errors['trello'] = "Not a trello url";
+
+            if(!validUrl($_POST['url']))
+                $errors['url'] = "Not a valid url";
+
             $f3->set('courseID', $_POST['courseID']);
             $f3->set('quarter', $_POST['quarter']);
             $f3->set('year', $_POST['year']);
+            $f3->set('github', $_POST['github']);
+            $f3->set('trello', $_POST['trello']);
+            $f3->set('url', $_POST['url']);
+            $f3->set('errors', $errors);
 
             if(empty($errors)) {
                 $_SESSION['courseID'] = $_POST['courseID'];
                 $_SESSION['quarter'] = $_POST['quarter'];
                 $_SESSION['year'] = $_POST['year'];
-
+                $_SESSION['github'] = $_POST['github'];
+                $_SESSION['trello'] = $_POST['trello'];
+                $_SESSION['url'] = $_POST['url'];
 
                 $f3->reroute('/'); // TODO: change route 
             }
--- models/new-class-validation.php
+++ models/new-class-validation.php
@@ -1,7 +1,7 @@
 <?php
 
 	function validCourseID($courseID) {
-		return ctype_alnum( (int) $courseID) && strlen($courseID) == 4);
+		return ctype_alnum( (int) $courseID) && strlen($courseID) == 4;
 	}
 
 	function validQuarter($quarter) {
@@ -10,9 +10,28 @@
 		return in_array($quarter, $validQuarters);
 	}
 
-	function validYear($year) {
+	function validDate($year) {
 		return $year >= date('Y');
 	}
+
+	function validGithubUrl($url) {
+		if(filter_var($url, FILTER_VALIDATE_URL)) {
+			if(parse_url($url, PHP_URL_HOST) == "github.com")
+				return true;
+		}
+
+		return false;
+	}
+
+	function validTrelloUrl($url) {
+		if(filter_var($url, FILTER_VALIDATE_URL)) {
+			if(parse_url($url, PHP_URL_HOST) == "trello.com")
+				return true;
+		}
+
+		return false;
+	}
+	
 
 	function validUrl($url) {
 		return filter_var($url, FILTER_VALIDATE_URL);
