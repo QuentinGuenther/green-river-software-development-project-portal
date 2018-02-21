d323e9a39d8071c3bdea2e85a4815a2988e0aa28
Nathan Corbin <ncorbin94@live.com>
2018-02-21 17:53:08 -0500

    Finish Class class

--- models/classes/class.php
+++ models/classes/class.php
@@ -9,10 +9,23 @@
 	 */
 	class Class extends Project
 	{
+		// course information
 		private $_courseId;
 		private $_quarter;
 		private $_year;
 		private $_instructor;
+
+		// project urls
+		private $_github;
+		private $_trello;
+		private $_url;
+		
+		// login credentials for the project
+		private $_username;
+		private $_password;
+
+		// notes
+		private $_instructorNotes;
 
 		/**
 		 * Creates a new class
@@ -56,7 +69,6 @@
 			return $this->year;
 		}
 
-
 		/**
 		 * Gets the instructor name
 		 * @return string
@@ -64,6 +76,64 @@
 		public function getInstructor()
 		{
 			return $this->instructor;
+		}
+
+		/**
+		 * Gets the project github url
+		 * @return string 
+		 */
+		public function getGithub()
+		{
+			return $this->github;
+		}
+
+		/**
+		 * Gets the project trello url
+		 * @return string 
+		 */
+		public function getTrello()
+		{
+			return $this->trello;
+		}
+
+		/**
+		 * Gets the project url
+		 * @return string 
+		 */
+		public function getUrl()
+		{
+			return $this->url;
+		}
+
+		/**
+		 * Gets the the username credential 
+		 * for the project website
+		 * 
+		 * @return string 
+		 */
+		public function getUsername()
+		{
+			return $this->username;
+		}
+
+		/**
+		 * Gets the the password credential 
+		 * for the project website
+		 * 
+		 * @return string 
+		 */
+		public function getPassword()
+		{
+			return $this->password;
+		}
+
+		/**
+		 * Gets the instructors notes
+		 * @return string
+		 */
+		public function getInstructorNotes()
+		{
+			return $this->instructorNotes;
 		}
 
 		/**
@@ -100,5 +170,59 @@
 		public function setInstructor($instructor)
 		{
 			$this->instructor = $instructor;
+		}
+
+		/**
+		 * Sets the github url
+		 * @param string $github the github url
+		 */
+		public function setGithub($github)
+		{
+			$this->github = $github;
+		}
+
+		/**
+		 * Sets the trello url
+		 * @param string $trello the trello url
+		 */
+		public function setTrello($trello)
+		{
+			$this->trello = $trello;
+		}
+
+		/**
+		 * Sets the project url
+		 * @param string $url the project url
+		 */
+		public function setUrl($url)
+		{
+			$this->url = $url;
+		}
+
+		/**
+		 * Sets the login username credential for the project
+		 * @param string $username the username to login
+		 */
+		public function setUsername($username)
+		{
+			$this->username = $username;
+		}
+
+		/**
+		 * Sets the login username credential for the project
+		 * @param string $username the username to login
+		 */
+		public function setPassword($password)
+		{
+			$this->password = $password;
+		}
+
+		/**
+		 * Sets the instructors notes
+		 * @param string $notes the notes to set
+		 */
+		public function setInstructorNotes($instructorNotes)
+		{
+			$this->instructorNotes = $instructorNotes;
 		}
 	}
